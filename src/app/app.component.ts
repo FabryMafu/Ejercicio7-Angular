5d0a4d4ad3f525986a2a00aaefff196ef0dfb38c
Fabricio Mafuresco <f.mafu@hotmail.com>
2024-11-15 17:36:47 -0300

    Ejercicio Hecho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { JsonService } from './services/json.service';
+import { Post } from './post';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'ejercicio7';
+export class AppComponent implements OnInit {
+  posts: Post[] = [];
+
+  constructor(private postService: JsonService) { }
+
+  ngOnInit() {
+    this.postService.getPosts().subscribe(data => {
+      this.posts = data;
+    });
+  }
 }
